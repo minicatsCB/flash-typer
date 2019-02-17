6c9fcc2cef24629028501ad13d016db718241871
Maribel <minicatsCB@users.noreply.github.com>
2019-02-17 16:54:48 +0100

    Add main screen title and available actions

--- scenes/main.js
+++ scenes/main.js
@@ -10,6 +10,26 @@ class Main extends Phaser.Scene {
     }
 
     create() {
+        let titleText = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 4, 'Flash Typer', {
+            fontSize: '32px',
+            fill: '#fff'
+        }).setOrigin(0.5, 0.5);
+
+        let instructionsText = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 4 + 50, 'Choose an option by typing it', {
+            fontSize: '16px',
+            fill: '#fff'
+        }).setOrigin(0.5, 0.5);
+
+        let playText = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 4 + 100, 'play', {
+            fontSize: '32px',
+            fill: '#fff'
+        }).setOrigin(0.5, 0.5);
+
+        let rankingText = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 4 + 150, 'ranking', {
+            fontSize: '32px',
+            fill: '#fff'
+        }).setOrigin(0.5, 0.5);
+
         this.input.manager.enabled = true;
         this.input.once('pointerdown', function() {
             this.scene.start('game');
