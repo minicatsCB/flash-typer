96fd3643a90803705641a045633f08c75acada31
Maribel <minicatsCB@users.noreply.github.com>
2019-02-18 11:19:11 +0100

    Generate a poster after certain interval of time instead of all at once

--- scenes/game.js
+++ scenes/game.js
@@ -1,4 +1,5 @@
-let wordsObject;
+let wordsObject = {};
+let combos = {};
 let score = 0;
 let scoreText;
 let lives = 3;
@@ -48,20 +49,12 @@ class Game extends Phaser.Scene {
     }
 
     create() {
-        wordsObject = this.createWordsObject(currentLevel.wordList);
         let allPosters = [];
-        let combos = {};
-        for(let word in wordsObject) {
-            this.input.keyboard.createCombo(word);
-            let poster = this.createPoster(word, this);
 
-            // Associate each combo with a poster.
-            // This way, we can destroy the corresponding poster when
-            // WHOLE a word is typed, this is, a combo is matched
-            combos[word] = poster;
-
-            allPosters.push(poster);
-        }
+        let iter = this.createPoster(this);
+        setInterval(() => {
+            console.log(iter.next());
+        }, 1000);
 
         // The text to be shown in the score area
         scoreText = this.add.text(16, 16, 'Score: ' + score, {
@@ -113,17 +106,6 @@ class Game extends Phaser.Scene {
 
     }
 
-    createWordsObject(words){
-        // If the word has already been typed, then it is NOT alive (false)
-        // If the word has not been typed yed, then it is alive (true). Default value
-        let obj = {};
-        for(let word of words) {
-            obj[word] = { isAlive: true };
-        }
-
-        return obj;
-    }
-
     posterCollided(bottomCollider, poster) {
         // NOTE: We know it's position 1 of the array because when we created
         // the poster container, we added the image first and the text secondly
@@ -135,35 +117,46 @@ class Game extends Phaser.Scene {
     }
 
     // A poster is an image and the specific word put together as a group
-    createPoster(word, that) {
-        let posterImage = that.add.image(0, 0, "paper");
-        let posterText = that.add.text(0, 0, word, {
-            fontSize: "32px",
-            fill: "#000",
-            padding: 20
-        });
+    *createPoster(that) {
+        let index = 0;
+        for(let word of currentLevel.wordList) {
+            wordsObject[word] = { isAlive: true };
+            let posterImage = that.add.image(0, 0, "paper");
+            let posterText = that.add.text(0, 0, word, {
+                fontSize: "32px",
+                fill: "#000",
+                padding: 20
+            });
 
-        // Center the text on the image
-        posterText.setOrigin(0.5, 0.5);
+            // Center the text on the image
+            posterText.setOrigin(0.5, 0.5);
 
-        // Set the size of the image to the same size of the text
-        posterImage.displayWidth = posterText.getBounds().width;
-        posterImage.displayHeight = posterText.getBounds().height;
+            // Set the size of the image to the same size of the text
+            posterImage.displayWidth = posterText.getBounds().width;
+            posterImage.displayHeight = posterText.getBounds().height;
 
-        // Put the poster randomly at the canvas top
-        let randomX = Phaser.Math.Between(0, that.game.canvas.width);
-        let randomY = Phaser.Math.Between(-(posterImage.displayHeight / 2), -(posterImage.displayHeight) * 2);
+            // Put the poster randomly at the canvas top
+            let randomX = Phaser.Math.Between(0, that.game.canvas.width);
+            let randomY = Phaser.Math.Between(-(posterImage.displayHeight / 2), -(posterImage.displayHeight) * 2);
 
-        // Create the poster itself by grouping both image and text in a "container"
-        let container = that.add.container(randomX, randomY, [posterImage, posterText]);
+            // Create the poster itself by grouping both image and text in a "container"
+            let container = that.add.container(randomX, randomY, [posterImage, posterText]);
 
-        // Set the size of the container to the same size of the text (same as the image's)
-        container.setSize(posterImage.displayWidth, posterImage.displayHeight);
+            // Set the size of the container to the same size of the text (same as the image's)
+            container.setSize(posterImage.displayWidth, posterImage.displayHeight);
 
-        // Apply physics to the container
-        that.physics.world.enable(container);
-        container.body.setVelocity(0, currentLevel.posterVelocity);
+            // Apply physics to the container
+            that.physics.world.enable(container);
+            container.body.setVelocity(0, currentLevel.posterVelocity);
 
-        return container;
+            this.input.keyboard.createCombo(word);
+            //Associate each combo with a poster.
+            // This way, we can destroy the corresponding poster when
+            // WHOLE a word is typed, this is, a combo is matched
+            combos[word] = container;
+
+            index++;
+            yield container;
+        }
     }
 }
