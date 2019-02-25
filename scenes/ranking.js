e5c52bf207b2cac024b6d662b954eab0e0e22314
Maribel <minicatsCB@users.noreply.github.com>
2019-02-25 14:24:00 +0100

    Add Ranking scene title

--- scenes/ranking.js
+++ scenes/ranking.js
@@ -4,4 +4,18 @@ class Ranking extends Phaser.Scene {
             key: 'ranking'
         });
     }
+
+    preload() {
+
+    }
+
+    create() {
+        let canvasXMiddle = this.game.canvas.width / 2;
+        let rankingText = this.add.text(canvasXMiddle, 100, 'Ranking', {
+            fontSize: '32px',
+            fill: '#fff'
+        }).setOrigin(0.5, 0.5);
+    }
+
+    update() {}
 }
